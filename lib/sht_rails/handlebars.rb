ae0afc0bc0b67c903a131489835d05f45af43e18
Grant Klinsing <grant@grantrklinsing.com>
2014-03-04 14:20:56 -0700

    the runtime is not on the context

--- lib/sht_rails/handlebars.rb
+++ lib/sht_rails/handlebars.rb
@@ -8,8 +8,9 @@ module ShtRails
       @context = nil unless ActionView::Resolver.caching?
       @context ||= begin
         context = ::Handlebars::Context.new
+        runtime = context.instance_variable_get :@js
         if helpers = Rails.application.assets.find_asset(ShtRails.helper_path)
-          context.runtime.eval helpers.source
+          runtime.eval helpers.source
         end
         partials.each { |key, value| context.register_partial(key, value) } if partials
         context
